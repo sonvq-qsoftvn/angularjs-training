3a20a834f01ca2bf4b649ed936743de0a68ba7b0
Steve <sonvq@qsoftvietnam.com>
2016-05-24 13:43:10 +0700

    commit base source code

---
+++ myapp/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTrainingApp
+ * @description
+ * # angularTrainingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTrainingApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ myapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTrainingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTrainingApp
+ */
+angular.module('angularTrainingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
